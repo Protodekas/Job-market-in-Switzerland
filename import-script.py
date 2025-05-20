3eb835587e71d8fdc45c5168ae008803c8f07bfc
Protodekas <protodekas@gmail.com>
2025-05-20 21:49:49 +0200

    modified import-script.py

--- import-script.py
+++ import-script.py
@@ -2,8 +2,15 @@ import pandas as pd
 
 # loading files
 data = pd.read_csv("raw-data/kof_data_export_2025-05-13_14_27_46.csv")
-corr_dic = pd.read_csv("raw-data/kof_data_export_2025-05-13_14_30_15.csv")
+names = pd.read_csv("raw-data/kof_data_export_2025-05-13_14_30_15.csv")
+
+# correspondance dictionnary between data table and variable names table
+corr_dict = dict(zip(names["ts_key"], names["Variable"]))
+
+# rename the variables in the data table
+data_renamed = data.rename(columns={col: corr_dict[col] for col in data.columns if col in corr_dict})
 
 # diplaying the first lines
-print(f"raw-data/kof_data_export_2025-05-13_14_27_46.csv\n", data.head())
-print(f"raw-data/kof_data_export_2025-05-13_14_30_15.csv\n", corr_dic.head())
+# print(f"raw-data/kof_data_export_2025-05-13_14_27_46.csv\n", data.head())
+# print(f"raw-data/kof_data_export_2025-05-13_14_30_15.csv\n", names.head())
+print(data_renamed.head())
