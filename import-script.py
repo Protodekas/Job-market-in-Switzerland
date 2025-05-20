eca77e2a7d0afc3df245c234fef60a04ebd096c7
Protodekas <protodekas@gmail.com>
2025-05-20 22:27:20 +0200

    modified import-script.py

--- import-script.py
+++ import-script.py
@@ -10,7 +10,8 @@ corr_dict = dict(zip(names["ts_key"], names["Variable"]))
 # rename the variables in the data table
 data_renamed = data.rename(columns={col: corr_dict[col] for col in data.columns if col in corr_dict})
 
-# diplaying the first lines
+# diplaying the first lines and print the corrected datafile
 # print(f"raw-data/kof_data_export_2025-05-13_14_27_46.csv\n", data.head())
 # print(f"raw-data/kof_data_export_2025-05-13_14_30_15.csv\n", names.head())
 print(data_renamed.head())
+data_renamed.to_csv("corrected_data.csv", index=False)
