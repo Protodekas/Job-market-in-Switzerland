8e74dff74e5cf6fd653625ec14e9eb6c507a795e
Protodekas <protodekas@gmail.com>
2025-05-21 10:15:43 +0200

    updated import-script.py

--- import-script.py
+++ import-script.py
@@ -4,14 +4,16 @@ import pandas as pd
 data = pd.read_csv("raw-data/kof_data_export_2025-05-13_14_27_46.csv")
 names = pd.read_csv("raw-data/kof_data_export_2025-05-13_14_30_15.csv")
 
+# removing the unused columns from the data table
+cols_ok = names["ts_key"].tolist()
+data = data[["date"] + [col for col in data.columns if col in cols_ok]]
+
 # correspondance dictionnary between data table and variable names table
 corr_dict = dict(zip(names["ts_key"], names["Variable"]))
 
 # rename the variables in the data table
 data_renamed = data.rename(columns={col: corr_dict[col] for col in data.columns if col in corr_dict})
 
-# diplaying the first lines and print the corrected datafile
-# print(f"raw-data/kof_data_export_2025-05-13_14_27_46.csv\n", data.head())
-# print(f"raw-data/kof_data_export_2025-05-13_14_30_15.csv\n", names.head())
+# diplaying the first lines and print the corrected csv data file
 print(data_renamed.head())
 data_renamed.to_csv("corrected_data.csv", index=False)
