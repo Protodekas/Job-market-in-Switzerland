95fdd99f93c34938b4754ab6b4285e64772425df
Protodekas <protodekas@gmail.com>
2025-05-22 11:27:22 +0200

    updated analysis-script.py

--- analysis-script.py
+++ analysis-script.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import json
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # === import and cleaning of the data ===
 # loading files
@@ -23,13 +24,13 @@ data = data.rename(columns={col: corr_dict[col] for col in data.columns if col i
 data.to_csv("corrected_data.csv", index=False)
 
 # dividing data table in dataframes according to the groups
-cantons = names[names["Grouped by"] == "Canton"]["Variable"].tolist()
-jobs = names[names["Grouped by"] == "Occupation"]["Variable"].tolist()
-industries = names[names["Grouped by"] == "Industry"]["Variable"].tolist()
+cantons_cols = names[names["Grouped by"] == "Canton"]["Variable"].tolist()
+jobs_cols = names[names["Grouped by"] == "Occupation"]["Variable"].tolist()
+industries_cols = names[names["Grouped by"] == "Industry"]["Variable"].tolist()
 
-cantons = data[["date"] + [col for col in data.columns if col in cantons]]
-jobs = data[["date"] + [col for col in data.columns if col in jobs]]
-industries = data[["date"] + [col for col in data.columns if col in industries]]
+cantons = data[["date"] + [col for col in data.columns if col in cantons_cols]]
+jobs = data[["date"] + [col for col in data.columns if col in jobs_cols]]
+industries = data[["date"] + [col for col in data.columns if col in industries_cols]]
 
 # === descriptive statistics ===
 # stats by group
@@ -54,11 +55,48 @@ industries = data[["date"] + [col for col in data.columns if col in industries]]
 
 
 # === plotting the data ===
+# index by canton
+colors = sns.color_palette("Set1", 9) + sns.color_palette("Set2", 4) + sns.color_palette("Dark2", 8) + sns.color_palette("Paired", 5)
+colors[5] = sns.color_palette("tab20b")[1]
+
 cantons["date"] = pd.to_datetime(cantons["date"])
-cantons.set_index("date").plot(figsize=(14,6))
-plt.title("Evolution of job posting index by canton (2020 = 100)")
-plt.ylabel("Index")
+plt.figure(figsize=(16,8))
+for i, col in enumerate(cantons_cols):
+    plt.plot(cantons["date"], cantons[col], label=col, color=colors[i], linewidth=1)
+plt.title("Evolution of job posting index by canton")
+plt.ylabel("Index (1st week 2020 = 100)")
+plt.xlabel("Time")
 plt.grid(True)
-plt.legend(bbox_to_anchor = (1.05, 1), loc = "upper left")
+plt.legend(bbox_to_anchor=(1.05,1), loc="upper left", fontsize="small")
+plt.tight_layout()
+plt.show()
+
+# index by jobs
+colors = sns.color_palette("tab10")
+
+jobs["date"] = pd.to_datetime(jobs["date"])
+plt.figure(figsize=(16,8))
+for i, col in enumerate(jobs_cols):
+    plt.plot(jobs["date"], jobs[col], label=col, color=colors[i], linewidth=1)
+plt.title("Evolution of job posting index by job")
+plt.ylabel("Index (1st week 2020 = 100)")
+plt.xlabel("Time")
+plt.grid(True)
+plt.legend(bbox_to_anchor=(1.05,1), loc="upper left", fontsize="small")
+plt.tight_layout()
+plt.show()
+
+# index by industry
+colors = sns.color_palette("tab20")
+
+industries["date"] = pd.to_datetime(industries["date"])
+plt.figure(figsize=(16,8))
+for i, col in enumerate(industries_cols):
+    plt.plot(industries["date"], industries[col], label=col, color=colors[i], linewidth=1)
+plt.title("Evolution of job posting index by industry")
+plt.ylabel("Index (1st week 2020 = 100)")
+plt.xlabel("Time")
+plt.grid(True)
+plt.legend(bbox_to_anchor=(1.05,1), loc="upper left", fontsize="small")
 plt.tight_layout()
 plt.show()
